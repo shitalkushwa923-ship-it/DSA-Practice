cf82865798a48ad5ac90e17c17fc96838175f3f9
shitalkushwa923-ship-it <shitalkushwa923@gmail.com>
2026-03-16 22:23:53 +0530

    Create oddEvenNumber.cpp

---
+++ oddEvenNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int n;
+  cin>>n;
+  cout<<"Enter a number ";
+  if(n%==0){
+    cout<<"Even number"<<endl;
+  }
+  else{
+    cout<<"odd"<<endl;
+  }
+  retrun 0;
+}
